e742b4b384a66f4d300c657a446b7c66ab794118
YuanceYan <yyan0203@uni.sydney.edu.au>
2024-05-28 21:23:50 +1000

    Define the "Blackline"

--- sketch.js
+++ sketch.js
@@ -16,4 +16,33 @@ function setup() {
     // Resize the canvas when the window is resized
     resizeCanvas(windowWidth, windowHeight);
   }
+
+  class BlackLine {
+    constructor(x, y, w, h, adjustW) {
+      // x-coordinate
+      this.x = x;
+      // y-coordinate
+      this.y = y; 
+      // width of the line
+      this.w = w; 
+      // height of the line
+      this.h = h; 
+      // width adjustment
+      this.adjustW = adjustW;
+    }
+  
+    draw(hue) {
+      push();
+      // Translate to the position
+      translate(this.x, this.y);
+      // Rotate the line
+      rotate(-28);
+      noStroke(); 
+      // Fill color with hue
+      fill(hue, 100, 100); 
+      // Draw the rectangle
+      rect(0, 0, this.w + this.adjustW, this.h); 
+      pop();
+    }
+  }
   
